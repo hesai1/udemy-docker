d944d91c9aeb14b13d1478b870e677ca2b685c26
hesai1 <sai.he1@pactera.com>
2021-11-20 22:36:11 +0900

    update

---
+++ 5-38-cache/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dockerfilename=${1:-'dockerfile'}
+
+docker build -f ${dockerfilename} -t udemy-class:tmp .
+docker container rm -f $(docker container ls -f name=udemy-demo -q)
+docker run -d  --name udemy-demo -p 5000:5000 udemy-class:tmp
