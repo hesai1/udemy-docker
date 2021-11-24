2fd679d795991f40816ad9c216b640d571644fec
hesai1 <51629249+hesai1@users.noreply.github.com>
2021-11-24 09:37:52 +0900

    debug

--- 5-41-非root用户/build.sh
+++ 5-41-非root用户/build.sh
@@ -3,4 +3,4 @@ dockerfilename=${1:-'dockerfile'}
 
 docker build -f ${dockerfilename} -t udemy-class:tmp .
 docker container rm -f $(docker container ls -f name=udemy-demo -q)
-docker run -d  --name udemy-demo -p 5000:5000 udemy-class:tmpto
+docker run -d  --name udemy-demo -p 5000:5000 udemy-class:tmp
